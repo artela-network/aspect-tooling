d4608f08a9e857215bc2ef0d65074339b50ccbca
yuan <fatfishman90@gmail.com>
2023-07-05 17:42:35 +0800

    feat: add T for context get and set.

--- packages/libs/entry/context.ts
+++ packages/libs/entry/context.ts
@@ -11,6 +11,22 @@ export interface TraceCtx {
     getStateChanges(addr: string, variable: string, key: Uint8Array): StateChanges;
 }
 
+class ContextValue {
+    val : string;
+    constructor(val: string) {
+        this.val = val;
+    }
+    asI8(): i8 {return BigInt.fromString(this.val).toInt32() as i8;}
+    asU8(): u8 {return BigInt.fromString(this.val).toInt32() as u8;}
+    asI16(): i16 {return BigInt.fromString(this.val).toInt32() as i16;}
+    asU16(): u16 {return BigInt.fromString(this.val).toInt32() as u16;}
+    asI32(): i32 {return BigInt.fromString(this.val).toInt32();}
+    asU32(): u32 {return BigInt.fromString(this.val).toUInt32();}
+    asI64(): i64 {return BigInt.fromString(this.val).toInt64();}
+    asU64(): u64 {return BigInt.fromString(this.val).toUInt64();}
+
+}
+
 export class StateCtx {
     public getProperty(key: string): string {
         return Context.getProperty(key);
@@ -36,12 +52,42 @@ export class OnTxReceiveCtx {
         return Context.getProperty(key);
     }
 
-    public setContext(key: string, value: string): bool {
-        return Context.setContext(key, value);
-    }
+    // i8 / u8 / i16 / u16 / i32 / u32 / i64 / u64 / bool / string / bigint
+    public setContext<T>(key: string, value: T): bool {
+        let valueStr: string;
+        switch (typeof value) {
+            case 'bool':
+            case 'string':
+                valueStr = String(value);
+                break;
+            default:
+                valueStr = '';
+              break;
+          }
+          if (value instanceof BigInt) valueStr = value.toString();
+          // @ts-ignore
+          if (value instanceof i8) valueStr = BigInt.fromInt16(<i16>value).toString();
+          // @ts-ignore
+          if (value instanceof u8) valueStr = BigInt.fromUInt16(<u16>value).toString();
+          // @ts-ignore
+          if (value instanceof i16) valueStr = BigInt.fromInt16(value).toString();
+          // @ts-ignore
+          if (value instanceof u16) valueStr = BigInt.fromUInt16(value).toString();
+          // @ts-ignore
+          if (value instanceof i32) valueStr = BigInt.fromInt32(value).toString();
+          // @ts-ignore
+          if (value instanceof u32) valueStr = BigInt.fromUInt32(value).toString();
+          // @ts-ignore
+          if (value instanceof i64) valueStr = BigInt.fromInt64(value).toString();
+          // @ts-ignore
+          if (value instanceof u64) valueStr = BigInt.fromUInt64(value).toString();
 
-    public getContext(key: string): string {
-        return Context.getContext(key);
+        return Context.setContext(key, valueStr);
+    }
+    
+    public getContext(key: string): ContextValue {
+
+        return new ContextValue(Context.getContext(key));
     }
 
     public setAspectState(key: string, value: string): bool {
