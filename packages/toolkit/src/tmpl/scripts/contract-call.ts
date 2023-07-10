ac82afe72355205827e74ee687f3c05e75c184d0
zhanjun <zhanjun@artela.network>
2023-07-10 21:36:41 +0800

    fix: call error

--- packages/toolkit/src/tmpl/scripts/contract-call.ts
+++ packages/toolkit/src/tmpl/scripts/contract-call.ts
@@ -1,52 +1,88 @@
 export const ContractCallTmpl = `
 
+
 "use strict"
 
 // import required libs
 const fs = require('fs');
-const Web3 = require('web3');
-const yargs = require('yargs/yargs');
+const Web3 = require('@artela/web3');
+var argv = require('yargs')
+    .string('node')
+    .string('account')
+    .string('args')
+    .string('contract')
+    .string('inputs')
+    .string('method')
+    .string('abi')
+    .argv;
 
-// parse args
-const argv = yargs(process.argv.slice(2)).string('account').string('gasPrice').string('args').string('contract').argv;
-const method = argv.method;
-const contract = argv.contract;
-const abi = JSON.parse(fs.readFileSync(argv.abi, "utf-8").toString());
-const args = JSON.parse(argv.args);
-let account = fs.readFileSync(argv.account, "utf-8").toString();
-const contractOptions = {
-    gasPrice: argv.gasPrice || '1000000000',
-    gas: parseInt(argv.gas) || 4000000
-};
-const projectConfig = JSON.parse(fs.readFileSync('./project.config.json', "utf-8").toString());
 
 async function call() {
     // init connection to Artela node
-    const web3 = new Web3(projectConfig.node);
+    const configJson = JSON.parse(fs.readFileSync('./project.config.json', "utf-8").toString());
+    // init connection to Artela node
+    let node = (argv.node)?String(argv.node):configJson.node;
+    if(!node){
+        console.log("'node' cannot be empty, please set by the parameter or artela.config.json")
+        process.exit(0)
+    }
+    const web3 = new Web3(node);
 
-    if (!account) {
-        // try retrieve account from config
-        account = projectConfig.accounts && projectConfig.accounts.length > 0 ? projectConfig.accounts[0] : undefined;
-        if (!account) {
-            // try retrieve accounts from node
-            let accounts = await web3.eth.getAccounts();
-            account = accounts[0];
-        }
+
+    const account =String(argv.account)
+    if(!account || account==='undefined') {
+        console.log("'account' cannot be empty, please set by the parameter ' --account 0x9999999999999999999999999999999999999999'")
+        process.exit(0)
     }
 
+    // --args '{"gasPrice":1000000010,"gas":4000000}'
+    let argsJson =String(argv.args)
+    let gasPrice=null
+    let gas=null
+    if(argsJson && argsJson!=='undefined') {
+        let parseJson = JSON.parse(argsJson);
+        if (parseJson) {
+            gasPrice = parseJson.gasPrice
+            gas = parseJson.gas
+        }
+    }
+    const contractOptions = {
+        gasPrice: gasPrice || '1000000000',
+        gas: parseInt(gas) || 4000000
+    };
+
+    const contractAddr = argv.contract;
+    if(!contractAddr || contractAddr==='undefined') {
+        console.log("'contractAddr' cannot be empty, please set by the parameter ' --contract 0x9999999999999999999999999999999999999999'")
+        process.exit(0)
+    }
+    const abiPath=String(argv.abi)
+    let abi=null
+    if(abiPath && abiPath!=='undefined') {
+         abi = JSON.parse(fs.readFileSync(abiPath, "utf-8").toString());
+    }
+
+    const inputs = argv.inputs;
+    let parameters=[];
+    if(inputs && inputs!=='undefined') {
+        parameters = JSON.parse(inputs);
+    }
+    const method = argv.method;
+
     // retrieve current nonce
-    const nonceVal = await web3.eth.getTransactionCount(account);
+    const nonceVal = await web3.atl.getTransactionCount(account);
 
     // instantiate an instance of contract
-    let contract = new web3.atl.Contract(abi, contract);
-    
+    let contract = new web3.atl.Contract(abi, contractAddr);
+
     // invoke contract method
-    let instance = contract.methods[method](...args).send({ from: account, nonce: nonceVal, ...contractOptions });
+    let instance = contract.methods[method](...parameters).send({ from: account, nonce: nonceVal, ...contractOptions });
     contract = await instance.on('receipt', function (receipt) {
         console.log("receipt: ", receipt);
     }).on('transactionHash', (txHash) => {
         console.log("tx hash: ", txHash);
     });
+
 }
 
 call().then();
