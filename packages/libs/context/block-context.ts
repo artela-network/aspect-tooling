8399cfe916f3da6615e5d8d4ce5845163d0bc143
Jack Li <jack@artela.network>
2023-09-20 15:58:33 +0800

    feat: support new state tracing

--- packages/libs/context/block-context.ts
+++ packages/libs/context/block-context.ts
@@ -1,62 +1,63 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 
-import {DataSpaceType, EthBlockHeader, Evidence, LastCommitInfo, MinGasPrice, GasMeter, EthTxArray} from "../proto";
-import {Protobuf} from "as-proto/assembly";
-import {RuntimeContextAccessor} from "../system";
+import {
+  DataSpaceType,
+  EthBlockHeader,
+  Evidence,
+  LastCommitInfo,
+  MinGasPrice,
+  GasMeter,
+  EthTxArray,
+} from '../proto';
+import { Protobuf } from 'as-proto/assembly';
+import { RuntimeContextAccessor } from '../system';
 
 export class BlockContext {
-
-
-    public blockHeader(): EthBlockHeader | null {
-
-        const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_HEADER, null);
-        if (!response.data.value) {
-            return null
-        }
-        return Protobuf.decode<EthBlockHeader>(response.data.value, EthBlockHeader.decode);
-
+  public blockHeader(): EthBlockHeader | null {
+    const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_HEADER, null);
+    if (!response.data.value) {
+      return null;
     }
+    return Protobuf.decode<EthBlockHeader>(response.data.value, EthBlockHeader.decode);
+  }
 
-    public getSimilarTxs(): EthTxArray | null {
-        const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_TXS, null);
-        if (!response.data.value) {
-            return null
-        }
-        return Protobuf.decode<EthTxArray>(response.data.value, EthTxArray.decode);
-
+  public getSimilarTxs(): EthTxArray | null {
+    const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_TXS, null);
+    if (!response.data.value) {
+      return null;
     }
+    return Protobuf.decode<EthTxArray>(response.data.value, EthTxArray.decode);
+  }
 
-    public gasMeter(): GasMeter | null {
-        const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_GAS_METER, null);
-        if (!response.data.value) {
-            return null
-        }
-        return Protobuf.decode<GasMeter>(response.data.value, GasMeter.decode);
-
+  public gasMeter(): GasMeter | null {
+    const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_GAS_METER, null);
+    if (!response.data.value) {
+      return null;
     }
+    return Protobuf.decode<GasMeter>(response.data.value, GasMeter.decode);
+  }
 
-    public minGasPrice(): MinGasPrice | null {
-        const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_MIN_GAS_PRICE, null);
-        if (!response.data.value) {
-            return null
-        }
-        return Protobuf.decode<MinGasPrice>(response.data.value, MinGasPrice.decode);
+  public minGasPrice(): MinGasPrice | null {
+    const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_MIN_GAS_PRICE, null);
+    if (!response.data.value) {
+      return null;
     }
+    return Protobuf.decode<MinGasPrice>(response.data.value, MinGasPrice.decode);
+  }
 
-    public lastCommit(): LastCommitInfo | null {
-        const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_LAST_COMMIT, null);
-        if (!response.data.value) {
-            return null
-        }
-        return Protobuf.decode<LastCommitInfo>(response.data.value, LastCommitInfo.decode);
+  public lastCommit(): LastCommitInfo | null {
+    const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_LAST_COMMIT, null);
+    if (!response.data.value) {
+      return null;
     }
+    return Protobuf.decode<LastCommitInfo>(response.data.value, LastCommitInfo.decode);
+  }
 
-    public getEvidences(): Evidence | null {
-        const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_EVIDENCE, null);
-        if (!response.data.value) {
-            return null
-        }
-        return Protobuf.decode<Evidence>(response.data.value, Evidence.decode);
+  public getEvidences(): Evidence | null {
+    const response = RuntimeContextAccessor.get(DataSpaceType.BLOCK_EVIDENCE, null);
+    if (!response.data.value) {
+      return null;
     }
+    return Protobuf.decode<Evidence>(response.data.value, Evidence.decode);
+  }
 }
-
--- packages/libs/system/runtime-context-api.ts
+++ packages/libs/system/runtime-context-api.ts
@@ -1,65 +1,57 @@
-import {
-    ContextQueryRequest,
-    ContextQueryResponse,
-    DataSpaceType,
-    StringData
-} from "../proto";
-import {ABool, AString, AUint8Array} from "../types";
-import {ContextValue, ToString} from "./common";
-import {Protobuf} from 'as-proto/assembly';
+import { ContextQueryRequest, ContextQueryResponse, DataSpaceType, StringData } from '../proto';
+import { ABool, AString, AUint8Array } from '../types';
+import { ContextValue, ToString } from './common';
+import { Protobuf } from 'as-proto/assembly';
 
 declare namespace __RuntimeContextApi__ {
-    function get(query: i32): i32
+  function get(query: i32): i32;
 
-    function setAspectContext(key: i32, value: i32): i32
+  function setAspectContext(key: i32, value: i32): i32;
 }
 
 class RuntimeContext {
-    public get(dataSpace: DataSpaceType, keys: Array<string>): ContextQueryResponse | null {
-        const contextQueryRequest = new ContextQueryRequest(dataSpace, keys);
-        const encoded = Protobuf.encode(contextQueryRequest, ContextQueryRequest.encode);
-        const input = new AUint8Array();
-        input.set(encoded);
-        const inputPtr = input.store();
-        const ret = __RuntimeContextApi__.get(inputPtr);
-        const bytes = new AUint8Array();
-        bytes.load(ret);
-        return Protobuf.decode<ContextQueryResponse>(bytes.get(), ContextQueryResponse.decode);
-    }
+  public get(dataSpace: DataSpaceType, keys: Array<string>): ContextQueryResponse | null {
+    const contextQueryRequest = new ContextQueryRequest(dataSpace, keys);
+    const encoded = Protobuf.encode(contextQueryRequest, ContextQueryRequest.encode);
+    const input = new AUint8Array();
+    input.set(encoded);
+    const inputPtr = input.store();
+    const ret = __RuntimeContextApi__.get(inputPtr);
+    const bytes = new AUint8Array();
+    bytes.load(ret);
+    return Protobuf.decode<ContextQueryResponse>(bytes.get(), ContextQueryResponse.decode);
+  }
 }
 
-
 export class AspectContext {
-    public get(key: string, aspectId: string = "", contractAddr: string = ""): ContextValue | null {
-        const array = new Array<string>(3);
-        array[0] = key;
-        array[1] = aspectId;
-        array[2] = contractAddr;
-        const response = RuntimeContextAccessor.get(DataSpaceType.TX_ASPECT_CONTEXT, array);
-        if (!response!.result!.success) {
-            return new ContextValue("")
-        } else {
-            const stringData = Protobuf.decode<StringData>(response!.data!.value, StringData.decode);
-            return new ContextValue(stringData.data)
-        }
+  public get(key: string, aspectId: string = '', contractAddr: string = ''): ContextValue | null {
+    const array = new Array<string>(3);
+    array[0] = key;
+    array[1] = aspectId;
+    array[2] = contractAddr;
+    const response = RuntimeContextAccessor.get(DataSpaceType.TX_ASPECT_CONTEXT, array);
+    if (!response!.result!.success) {
+      return new ContextValue('');
+    } else {
+      const stringData = Protobuf.decode<StringData>(response!.data!.value, StringData.decode);
+      return new ContextValue(stringData.data);
     }
+  }
 
-    public set<T>(key: string, value: T): bool {
-        const inputKey = new AString();
-        inputKey.set(key);
-        const inPtr = inputKey.store();
+  public set<T>(key: string, value: T): bool {
+    const inputKey = new AString();
+    inputKey.set(key);
+    const inPtr = inputKey.store();
 
-        const inputValue = new AString();
-        const dataStr = ToString(value);
-        inputValue.set(dataStr);
-        const ptrValue = inputValue.store();
-        const outPtr = __RuntimeContextApi__.setAspectContext(inPtr, ptrValue);
-        const output = new ABool();
-        output.load(outPtr);
-        return output.get();
-    }
+    const inputValue = new AString();
+    const dataStr = ToString(value);
+    inputValue.set(dataStr);
+    const ptrValue = inputValue.store();
+    const outPtr = __RuntimeContextApi__.setAspectContext(inPtr, ptrValue);
+    const output = new ABool();
+    output.load(outPtr);
+    return output.get();
+  }
 }
 
 export const RuntimeContextAccessor = new RuntimeContext();
-
-
