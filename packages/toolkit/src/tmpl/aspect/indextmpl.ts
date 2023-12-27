c837c9820e85ca9ffbbd37486a625f647ae0d10e
Jack Li <jack@artela.network>
2023-12-27 12:44:57 +0800

    chore: update generator with new lib

--- packages/toolkit/src/tmpl/aspect/indextmpl.ts
+++ packages/toolkit/src/tmpl/aspect/indextmpl.ts
@@ -4,9 +4,7 @@ import {
     entryPoint,
     execute,
     IPostContractCallJP,
-    IPreContractCallJP,
-    PostContractCallCtx,
-    PreContractCallCtx,
+    PostContractCallInput,
 } from "@artela/aspect-libs";
 
 /**
@@ -15,37 +13,26 @@ import {
  * About the concept of Aspect @see [join-point](https://docs.artela.network/develop/core-concepts/join-point)
  * How to develop an Aspect  @see [Aspect Structure](https://docs.artela.network/develop/reference/aspect-lib/aspect-structure)
  */
-class Aspect implements IPreContractCallJP, IPostContractCallJP {
+class Aspect implements IPostContractCallJP {
 
     /**
      * isOwner is the governance account implemented by the Aspect, when any of the governance operation
      * (including upgrade, config, destroy) is made, isOwner method will be invoked to check
      * against the initiator's account to make sure it has the permission.
      *
-     * @param ctx context of Aspect state
-     * @param sender address of the operation initiator
+     * @param sender address of the transaction
      * @return true if check success, false if check fail
      */
-    isOwner(sender: string): bool {
-        return true;
+    isOwner(sender: Uint8Array): bool {
+        return false;
     }
 
     /**
-     * preContractCall is a join-point which will be invoked before the contract call is executed.
+     * postContractCall is a join-point which will be invoked after a contract call has finished.
      *
-     * @param ctx context of the given join-point
-     * @return result of Aspect execution
+     * @param input input to the current join point
      */
-    preContractCall(ctx: PreContractCallCtx): void {
-    }
-
-    /**
-     * postContractCall is a join-point which will be invoked after a contract has finished.
-     *
-     * @param ctx context of the given join-point
-     * @return result of Aspect execution
-     */
-    postContractCall(ctx: PostContractCallCtx): void {
+    postContractCall(input: PostContractCallInput): void {
         // Implement me...
     }
 }
@@ -55,8 +42,6 @@ const aspect = new Aspect()
 entryPoint.setAspect(aspect)
 
 // 3.must export it
-export {execute, allocate}
-
-
+export { execute, allocate }
 
 `;
