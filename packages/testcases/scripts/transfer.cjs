5c2b3f385b05b10e3bf611ed6d462840261b1fb0
zhanjun <zhanjun@artela.network>
2023-12-28 10:36:07 +0800

    fix: transfer

--- packages/testcases/scripts/transfer.cjs
+++ packages/testcases/scripts/transfer.cjs
@@ -17,13 +17,15 @@ async function f() {
         console.log("'node' cannot be empty, please set by the parameter or artela.config.json")
         process.exit(0)
     }
+    console.log("node: " + node)
+
     const web3 = new Web3(node);
 
     let privateFile = String(argv.skfile)
     if (!privateFile || privateFile === 'undefined') {
         privateFile = "privateKey.txt"
     }
-    console.log("=== " + privateFile)
+    console.log("privateKey :file " + privateFile)
     let account;
     if (fs.existsSync(privateFile)) {
         let pk = fs.readFileSync(privateFile, 'utf-8');
@@ -32,14 +34,11 @@ async function f() {
         console.log("invalid private key")
         process.exit(0)
     }
-
     // add account to wallet
     web3.atl.accounts.wallet.add(account.privateKey);
-    console.log("address: ", account.address);
+    console.log("receiver address: ", account.address);
 
     const receiver = account.address;
-
-
 
     let accounts = await web3.eth.getAccounts();
     let senderAddr = accounts[0]
@@ -47,12 +46,13 @@ async function f() {
     let from = String(argv.from)
     if (from && from!=="undefined") {
         let sender = web3.eth.accounts.privateKeyToAccount(from);
+        web3.atl.accounts.wallet.add(sender.privateKey);
         senderAddr = sender.address
     }
 
     // retrieve current nonce
     const balance = await web3.eth.getBalance(senderAddr);
-    console.log('===sender balance:' + balance);
+    console.log('sender: '+senderAddr+' ' + balance);
 
 
     // transfer account from bank to local account
@@ -66,7 +66,6 @@ async function f() {
         'gaslimit': 4000000,
         'nonce': bankNonce
     };
-
     // send transaction
     await web3.atl.sendTransaction(tx1).on('receipt', receipt => {
         console.log('transferred from bank to local account');
