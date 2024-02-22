cac3b088594eac6e6e4cef6a1909d32d96926790
zhanjun <zhanjun@artela.network>
2024-02-22 14:50:59 +0800

    fix: update version

--- packages/testcases/tests/operation-aspect.test.js
+++ packages/testcases/tests/operation-aspect.test.js
@@ -2,21 +2,21 @@ import {ConnectToANode, DeployAspect, EntryPoint} from "./bese-test.js";
 
 import assert from "assert";
 
-//将数值写入到视图中，获得其字节数组，大端字节序
+//Write the numeric value into the view to get its byte array, big-endian endian
 function getUint8Array(len, setNum) {
-    var buffer = new ArrayBuffer(len);  //指定字节长度
-    setNum(new DataView(buffer));  //根据不同的类型调用不同的函数来写入数值
-    return new Uint8Array(buffer); //创建一个字节数组，从缓存中拿取数据
+    var buffer = new ArrayBuffer(len);  //specify the byte length
+    setNum(new DataView(buffer));  //Depending on the type, different functions are called to write values
+    return new Uint8Array(buffer); //create a byte array to fetch data from the cache
 }
-//得到一个8位有符号整型的字节数组，大端字节序
+//Get an 8-bit signed integer byte array, big-endian endian
 function getInt8Bytes(num) {
     return getUint8Array(1, function (view) { view.setInt8(0, num); })
 }
-//得到一个8位无符号整型的字节数组，大端字节序
+//Get an 8-bit unsigned integer byte array with big-endian endianism
 function getUint8Bytes(num) {
     return getUint8Array(1, function (view) { view.setUint8(0, num); })
 }
-//得到一个16位有符号整型的字节数组，大端字节序
+//Get a 16-bit signed integer byte array with big-endian entitlement
 function getInt16Bytes(num) {
     return getUint8Array(2, function (view) { view.setInt16(0, num); })
 }
