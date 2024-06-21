c34fd80d7b24677643d3e4ae3eab50d70656541c
Luke <129730838+luketheart@users.noreply.github.com>
2024-06-21 10:45:55 +0800

    feat/evm cryptos (#36)
    * fix: add proto for evm crypto methods
    
    * fix: evm crytpo api methods
    
    * fix: add G1 and G2 for encoding params
    
    * fix: add uint256
    
    * fix: remove parseInt
    
    * fix: bigModeExp use uint8array
    
    * fix: bn256Pairing result check
    
    * fix: ecRecover
    
    * fix: optimize bytes to hex
    
    * fix: blake2F
    
    * fix: Adjust the structure of blake2F to avoid the Array<u64> encoding problem of as proto

--- packages/libs/hostapi/crypto-api.ts
+++ packages/libs/hostapi/crypto-api.ts
@@ -1,4 +1,10 @@
-import { AUint8Array, BigInt, hexToUint8Array, uint8ArrayToHex } from '../common';
+import { Protobuf } from 'as-proto/assembly';
+import { ABool, AUint8Array, Uint256, hexToUint8Array, uint8ArrayToHex } from '../common';
+import { Bn256AddInput, Bn256PairingInput, G1, G2 } from '../proto'
+import { Bn256ScalarMulInput } from '../proto/aspect/v2/bn256scalar-mul-input';
+import { G1Point, G2Point, ethereum } from '../common'
+import { EcRecoverInput } from '../proto/aspect/v2/ec-recover-input';
+import { Blake2FInput } from '../proto/aspect/v2/blake2finput';
 
 declare namespace __CryptoApi__ {
   function sha256(dataPtr: i32): i32;
@@ -8,12 +14,22 @@ declare namespace __CryptoApi__ {
   function keccak(dataPtr: i32): i32;
 
   function ecRecover(dataPtr: i32): i32;
+
+  function bigModExp(basePtr: i32, expPtr: i32, modPtr: i32): i32;
+
+  function bn256Add(dataPtr: i32): i32;
+
+  function bn256ScalarMul(dataPtr: i32): i32;
+
+  function bn256Pairing(dataPtr: i32): i32;
+
+  function blake2F(dataPtr: i32): i32;
 }
 
 export class CryptoApi {
   private static _ins: CryptoApi | null = null;
 
-  private constructor() {}
+  private constructor() { }
 
   public static instance(): CryptoApi {
     if (!CryptoApi._ins) {
@@ -56,25 +72,14 @@ export class CryptoApi {
    *
    * @returns string returns an address, and not an address payable
    */
-  public ecRecover(hash: string, v: BigInt, r: BigInt, s: BigInt): string {
-    if (
-      v.countBits() == 0 ||
-      r.countBits() == 0 ||
-      s.countBits() == 0 ||
-      v.countBits() > 256 ||
-      r.countBits() > 256 ||
-      s.countBits() > 256
-    ) {
-      return '';
-    }
-    const vStr = v.countBits() == 256 ? v.toString(16) : v.toString(16).padStart(64, '0');
-    const rStr = r.countBits() == 256 ? r.toString(16) : r.toString(16).padStart(64, '0');
-    const sStr = s.countBits() == 256 ? s.toString(16) : s.toString(16).padStart(64, '0');
+  public ecRecover(hash: Uint8Array, v: Uint256, r: Uint256, s: Uint256): Uint8Array {
+    const input = new EcRecoverInput(hash, v.toUint8Array(), r.toUint8Array(), s.toUint8Array());
+    const inputPtr = new AUint8Array(Protobuf.encode(input, EcRecoverInput.encode)).store();
 
-    //[msgHash 32B][v 32B][r 32B][s 32B]
-    const syscallInput = hash + vStr + rStr + sStr;
-    const ret = this._ecRecover(hexToUint8Array(syscallInput));
-    return uint8ArrayToHex(ret);
+    const ret = __CryptoApi__.ecRecover(inputPtr);
+    const resRaw = new AUint8Array();
+    resRaw.load(ret);
+    return resRaw.body;
   }
 
   private _ecRecover(data: Uint8Array): Uint8Array {
@@ -83,5 +88,125 @@ export class CryptoApi {
     const resRaw = new AUint8Array();
     resRaw.load(resPtr);
     return resRaw.body;
+  }
+
+  /**
+   * bigModExp implements a native big integer exponential modular operation.
+   * @param base
+   * @param exp
+   * @param mod
+   *
+   * @returns
+   */
+  public bigModExp(base: Uint8Array, exp: Uint8Array, mod: Uint8Array): Uint8Array {
+    const basePtr = new AUint8Array(base).store();
+    const expPtr = new AUint8Array(exp).store();
+    const modPtr = new AUint8Array(mod).store();
+    const resPtr = __CryptoApi__.bigModExp(basePtr, expPtr, modPtr);
+    const resRaw = new AUint8Array();
+    resRaw.load(resPtr);
+    return resRaw.body;
+  }
+
+  /**
+   * bn256Add implements a native elliptic curve point addition conforming to Istanbul consensus rules.
+   * @param a
+   * @param b
+   *
+   * @returns
+   */
+  public bn256Add(a: G1Point, b: G1Point): G1Point {
+    const input = new Bn256AddInput(
+      new G1(a.x.toUint8Array(), a.y.toUint8Array()),
+      new G1(b.x.toUint8Array(), b.y.toUint8Array()),
+    );
+    const inputPtr = new AUint8Array(Protobuf.encode(input, Bn256AddInput.encode)).store();
+
+    const resPtr = __CryptoApi__.bn256Add(inputPtr);
+    const resRaw = new AUint8Array();
+    resRaw.load(resPtr);
+    return new G1Point().decode(resRaw.get());
+  }
+
+  /**
+   * bn256ScalarMul implements a native elliptic curve scalar multiplication conforming to Istanbul consensus rules.
+   * @param p
+   * @param scalar
+   *
+   * @returns
+   */
+  public bn256ScalarMul(p: G1Point, scalar: Uint256): G1Point {
+    const input = new Bn256ScalarMulInput(
+      new G1(p.x.toUint8Array(), p.y.toUint8Array()),
+      scalar.toUint8Array(),
+    );
+    const inputPtr = new AUint8Array(Protobuf.encode(input, Bn256ScalarMulInput.encode)).store();
+
+    const resPtr = __CryptoApi__.bn256ScalarMul(inputPtr);
+    const resRaw = new AUint8Array();
+    resRaw.load(resPtr);
+    return new G1Point().decode(resRaw.get());
+  }
+
+  /**
+   * bn256Pairing implements a pairing pre-compile for the bn256 curve conforming to Istanbul consensus rules.
+   * @param input
+   *
+   * @returns
+   */
+  public bn256Pairing(g1Points: G1Point[], g2Points: G2Point[]): bool {
+    if (g1Points.length != g2Points.length) {
+      return false;
+    }
+
+    const cs: Array<G1> = [];
+    const ts: Array<G2> = [];
+
+    for (let i = 0; i < g1Points.length; i++) {
+      const c = new G1(g1Points[i].x.toUint8Array(), g1Points[i].y.toUint8Array());
+      const t = new G2(
+        g2Points[i].x[0].toUint8Array(),
+        g2Points[i].x[1].toUint8Array(),
+        g2Points[i].y[0].toUint8Array(),
+        g2Points[i].y[1].toUint8Array(),
+      );
+      cs.push(c);
+      ts.push(t);
+    }
+
+    let input = new Bn256PairingInput(cs, ts)
+    const inputPtr = new AUint8Array(Protobuf.encode(input, Bn256PairingInput.encode)).store();
+
+    const resPtr = __CryptoApi__.bn256Pairing(inputPtr);
+    const resRaw = new AUint8Array();
+    resRaw.load(resPtr);
+    if (resRaw.get().length != 32) {
+      return false
+    }
+    return resRaw.get().at(31) == 1;
+  }
+
+  /**
+   * blake2F implements blake2F to Istanbul consensus rules.
+   * @param h
+   * @param m
+   * @param t
+   * @param final
+   * @param rounds
+   * @returns
+   */
+  public blake2F(h: Uint8Array, m: Uint8Array, t: Uint8Array, final: bool, rounds: Uint8Array): Uint8Array {
+    if (h.length != 64 || m.length != 128 || t.length != 16 || rounds.length != 4) {
+      return new Uint8Array(0);
+    }
+
+    let input = new Blake2FInput(h, m, t, final, rounds);
+    const inputPtr = new AUint8Array(Protobuf.encode(input, Blake2FInput.encode)).store();
+
+    const resPtr = __CryptoApi__.blake2F(inputPtr);
+    const resRaw = new AUint8Array();
+    resRaw.load(resPtr);
+
+    return resRaw.get();
   }
 }
