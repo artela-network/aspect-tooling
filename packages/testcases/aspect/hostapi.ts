a90657c9785f929b26aa31b30318952fe21ec04f
luketheart <eric@artela.network>
2024-07-30 15:15:34 +0800

    fix: add state test case

--- packages/testcases/aspect/hostapi.ts
+++ packages/testcases/aspect/hostapi.ts
@@ -39,12 +39,13 @@ class HostApiAspect implements
   }
 
   verifyTx(input: TxVerifyInput): Uint8Array {
-    this.checkPoperty();
     return sys.aspect.mutableState.get<Uint8Array>('owner').unwrap();
   }
 
   operation(input: OperationInput): Uint8Array {
     this.checkPoperty();
+    this.checkState(true);
+    this.checkState(false);
     return input.callData;
   }
 
@@ -53,18 +54,55 @@ class HostApiAspect implements
   }
 
   postContractCall(_: PostContractCallInput): void {
-    this.checkPoperty();
+    this.checkState(false);
+    this.checkState(true);
+    this.checkState(false);
   }
 
   preTxExecute(_: PreTxExecuteInput): void {
-    this.checkPoperty();
   }
 
   postTxExecute(_: PostTxExecuteInput): void {
-    this.checkPoperty();
+  }
+
+  checkState(set: bool): void {
+    const test = uint8ArrayToHex(sys.aspect.property.get<Uint8Array>("test"));
+    sys.log("_____________test: " + test.toString());
+    if (test != "02") {
+      return
+    }
+    if (set) {
+      this.setState()
+    } else {
+      this.getState()
+    }
+  }
+
+  setState(): void {
+    const state1 = sys.aspect.mutableState.get<string>("state")
+    state1.set<string>("0x123");
+    this.assertStr("0x123", state1.unwrap());
+  }
+
+  getState(): void {
+    const state1 = sys.aspect.mutableState.get<string>("state");
+    this.assertStr("0x123", state1.unwrap());
   }
 
   checkPoperty(): void {
+    const test = uint8ArrayToHex(sys.aspect.property.get<Uint8Array>("test"));
+    sys.log("_____________test: " + test.toString());
+    if (test != "01") {
+      return
+    }
+    const testu64 = sys.aspect.property.get<u64>("test");
+    const testi64 = sys.aspect.property.get<i64>("test");
+    const testi32 = sys.aspect.property.get<i32>("test");
+    const testu32 = sys.aspect.property.get<u32>("test");
+    if (testu64 != 1 || testi64 != 1 || testi32 != 1 || testu32 != 1) {
+      sys.revert("aspect assert failed, expect test value 1, got " + testu64.toString());
+    }
+
     const prop1 = sys.aspect.property.get<Uint8Array>("prop-key1");
     this.assert("1234567890abcdef", prop1);
     const prop2 = sys.aspect.property.get<Uint8Array>("prop-key2");
@@ -73,8 +111,12 @@ class HostApiAspect implements
 
   assert(expect: string, actual: Uint8Array): void {
     const strAct = uint8ArrayToHex(actual);
-    if (expect != strAct) {
-      let msg = "aspect assert failed, expect " + expect + ", got " + strAct;
+    this.assertStr(expect, strAct);
+  }
+
+  assertStr(expect: string, actual: string): void {
+    if (expect != actual) {
+      let msg = "aspect assert failed, expect " + expect + ", got " + actual;
       sys.revert(msg);
       return;
     }
